ddb4233f47391e1fd66b1162c4843b08b3d1be1e
Arj2001 <arjunraju122001@gmail.com>
2022-09-26 17:08:08 +0530

    employee updated

--- src/com/nest/employee/Employee.java
+++ src/com/nest/employee/Employee.java
@@ -1,11 +1,14 @@
 package com.nest.employee;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Employee {
 
     public static void main(String[] args) {
         int choice;
+        String empName;
+        ArrayList<String> empList = new ArrayList<>();
         while (true){
             System.out.println("Select an Option: ");
             System.out.println("1. Add Employee");
@@ -18,10 +21,12 @@ public class Employee {
             switch (choice){
 
                 case 1:
-                    System.out.println("Add Employee");
+                    System.out.println("Enter the name of an employee");
+                    empName = in.next();
+                    empList.add(empName);
                     break;
                 case 2:
-                    System.out.println("View Employee");
+                    System.out.println(empList);
                     break;
                 case 3:
                     System.exit(0);
@@ -29,6 +34,4 @@ public class Employee {
 
         }
     }
-
-
 }
